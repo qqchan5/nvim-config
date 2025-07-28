a26ca1e2dd5fafa106e0a7238ccbe04b1a7c543b
qqchan5 <qqmore5@gmail.com>
2025-07-28 21:00:26 +0200

    Some initial customization

--- init.lua
+++ init.lua
@@ -1,8 +1,11 @@
-
 -- Set <space> as the leader key
 -- See `:help mapleader`
 -- NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]] See `:h vim.o`
 -- NOTE: You can change these options as you wish!
@@ -17,27 +20,51 @@ vim.o.number = true
 -- option above, see `:h number_relativenumber`
 vim.o.relativenumber = true
 
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.o.mouse = ''
+
+-- Don't show the mode, since it's already in the status line
+vim.o.showmode = false
+
 -- Sync clipboard between OS and Neovim. Schedule the setting after `UiEnter` because it can
 -- increase startup-time. Remove this option if you want your OS clipboard to remain independent.
 -- See `:help 'clipboard'`
-vim.api.nvim_create_autocmd('UIEnter', {
-  callback = function()
-    vim.o.clipboard = 'unnamedplus'
-  end,
-})
+-- vim.api.nvim_create_autocmd('UIEnter', {
+--   callback = function()
+--     vim.o.clipboard = 'unnamedplus'
+--   end,
+-- })
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.o.ignorecase = true
-vim.o.smartcase = true
+-- vim.o.ignorecase = true
+-- vim.o.smartcase = true
 
--- Highlight the line where the cursor is on
+-- Keep signcolumn on by default
+vim.o.signcolumn = 'yes'
+
+-- Configure how new splits should be opened
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+-- Highlight the line and column where the cursor is on
 vim.o.cursorline = true
+vim.o.cursorcolumn = true
+
+-- Preview substitutions live, as you type!
+vim.o.inccommand = 'split'
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.o.scrolloff = 10
 
 -- Show <tab> and trailing spaces
 vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s) See `:help 'confirm'`
@@ -45,8 +72,28 @@ vim.o.confirm = true
 
 -- [[ Set up keymaps ]] See `:h vim.keymap.set()`, `:h mapping`, `:h keycodes`
 
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
 -- Use <Esc> to exit terminal mode
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
+
+-- Toggle between tabwidth 2 and 4
+vim.keymap.set('n', '<leader>tt', function()
+  local current = vim.bo.shiftwidth
+  if current == 2 then
+    vim.bo.tabstop = 4
+    vim.bo.shiftwidth = 4
+    vim.bo.softtabstop = 4
+    print 'Tab width set to 4'
+  else
+    vim.bo.tabstop = 2
+    vim.bo.shiftwidth = 2
+    vim.bo.softtabstop = 2
+    print 'Tab width set to 2'
+  end
+end, { desc = '[T]oggle [t]ab width' })
 
 -- Map <A-j>, <A-k>, <A-h>, <A-l> to navigate between windows in any modes
 vim.keymap.set({ 't', 'i' }, '<A-h>', '<C-\\><C-n><C-w>h')
@@ -58,8 +105,16 @@ vim.keymap.set({ 'n' }, '<A-j>', '<C-w>j')
 vim.keymap.set({ 'n' }, '<A-k>', '<C-w>k')
 vim.keymap.set({ 'n' }, '<A-l>', '<C-w>l')
 
+-- Keybinds for managing tabs
+--
+-- See `:help tabpage` for more info
+vim.keymap.set('n', '<C-t><C-t>', ':tabnew<CR>', { silent = true, desc = 'Open new tab' })
+vim.keymap.set('n', '<C-t><C-w>', ':tabclose<CR>', { silent = true, desc = 'Close current tab' })
+vim.keymap.set('n', '<C-t><C-o>', ':tabonly<CR>', { silent = true, desc = 'Close all but current tab' })
+
 -- [[ Basic Autocommands ]].
 -- See `:h lua-guide-autocommands`, `:h autocmd`, `:h nvim_create_autocmd()`
+-- See `:help lua-guide-autocommands`
 
 -- Highlight when yanking (copying) text.
 -- Try it with `yap` in normal mode. See `:h vim.hl.on_yank()`
@@ -86,4 +141,4 @@ end, { desc = 'Print the git blame for the current line' })
 
 -- For example, to add the "nohlsearch" package to automatically turn off search highlighting after
 -- 'updatetime' and when going to insert mode
-vim.cmd('packadd! nohlsearch')
+-- vim.cmd('packadd! nohlsearch')
