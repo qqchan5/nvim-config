64ac7e2e90a2aa9903a404f4badbe11787c2eacc
qqchan5 <qqmore5@gmail.com>
2025-07-30 18:16:10 +0200

    Cleanup things that are not used

--- init.lua
+++ init.lua
@@ -26,15 +26,6 @@ vim.o.mouse = ''
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
 
--- Sync clipboard between OS and Neovim. Schedule the setting after `UiEnter` because it can
--- increase startup-time. Remove this option if you want your OS clipboard to remain independent.
--- See `:help 'clipboard'`
--- vim.api.nvim_create_autocmd('UIEnter', {
---   callback = function()
---     vim.o.clipboard = 'unnamedplus'
---   end,
--- })
-
 -- Enable break indent
 vim.o.breakindent = true
 
@@ -43,10 +34,6 @@ vim.o.expandtab = true
 
 -- Save undo history
 vim.o.undofile = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
--- vim.o.ignorecase = true
--- vim.o.smartcase = true
 
 -- Keep signcolumn on by default
 vim.o.signcolumn = 'yes'
