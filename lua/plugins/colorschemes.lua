2036334c7c44b06d6e6d7d433769bb0ff5f05aaa
qqchan5 <qqmore5@gmail.com>
2025-08-03 11:37:15 +0200

    Fix typo
    It's a bit embarasing, but `transparent` was misspelt. As a result, I
    did not think it did anything meaningful for me and I got rid of
    background color manually. Now that the spelling is correct, those extra
    settings are no longer needed.

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -5,19 +5,12 @@ return {
     priority = 1000,
     config = function ()
       require("tokyonight").setup {
-        tranparent = false,
+        transparent = true,
         styles = {
           comments = { italic = false },
         },
       }
       vim.cmd.colorscheme "tokyonight-night"
-      vim.cmd.highlight "Normal guibg=NONE ctermbg=NONE"
-      vim.cmd.highlight "SignColumn guibg=NONE"
-      vim.cmd.highlight "LineNr guibg=NONE"
-      vim.cmd.highlight "EndOfBuffer guibg=NONE"
-      vim.cmd.highlight "WinSeparator guibg=NONE"
-      vim.cmd.highlight "NormalNC guibg=NONE ctermbg=NONE"
-      vim.cmd.highlight "NormalSB guibg=NONE ctermbg=NONE"
     end,
   },
 }
